6be4e1e64ddf0e5e5d42fabd26034eab013815e5
gopichandGC <gopichandvadlamudi123@gmail.com>
2024-02-25 19:16:37 +0530

    Ec2 Servers Creation For Roboshop Project

--- 04-ec2/data.tf
+++ 04-ec2/data.tf
@@ -1,3 +1,22 @@
+data "aws_ami" "centos8"{
+    owners = ["973714476881"]
+    most_recent      = true
+
+    filter {
+        name   = "name"
+        values = ["Centos-8-DevOps-Practice"]
+    }
+
+    filter {
+        name   = "root-device-type"
+        values = ["ebs"]
+    }
+
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
+}
 data "aws_ssm_parameter" "mongodb_sg_id" {
   name = "/${var.project_name}/${var.environment}/mongodb_sg_id"
 }
