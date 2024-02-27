5f32b6d5e4bf2f84c005a945cfe5a65e36a8a5c8
gopichandGC <gopichandvadlamudi123@gmail.com>
2024-02-27 20:25:09 +0530

    Ec2 Servers Creation For Roboshop Project

--- 04-ec2/ec2.tf
+++ 04-ec2/ec2.tf
@@ -69,3 +69,111 @@ module "rabbitmq" {
     }
   )
 }
+
+module "catalogue" {
+  source                 = "terraform-aws-modules/ec2-instance/aws"
+  ami = data.aws_ami.centos8.id
+  name                   = "${local.ec2_name}-catalogue"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [data.aws_ssm_parameter.catalogue_sg_id.value]
+  subnet_id              = local.private_subnet_id
+  tags = merge(
+    var.common_tags,
+    {
+      Component = "catalogue"
+    },
+    {
+      Name = "${local.ec2_name}-catalogue"
+    }
+  )
+}
+
+module "user" {
+  source                 = "terraform-aws-modules/ec2-instance/aws"
+  ami = data.aws_ami.centos8.id
+  name                   = "${local.ec2_name}-user"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [data.aws_ssm_parameter.user_sg_id.value]
+  subnet_id              = local.private_subnet_id
+  tags = merge(
+    var.common_tags,
+    {
+      Component = "user"
+    },
+    {
+      Name = "${local.ec2_name}-user"
+    }
+  )
+}
+
+module "cart" {
+  source                 = "terraform-aws-modules/ec2-instance/aws"
+  ami = data.aws_ami.centos8.id
+  name                   = "${local.ec2_name}-cart"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [data.aws_ssm_parameter.cart_sg_id.value]
+  subnet_id              = local.private_subnet_id
+  tags = merge(
+    var.common_tags,
+    {
+      Component = "cart"
+    },
+    {
+      Name = "${local.ec2_name}-cart"
+    }
+  )
+}
+
+module "shipping" {
+  source                 = "terraform-aws-modules/ec2-instance/aws"
+  ami = data.aws_ami.centos8.id
+  name                   = "${local.ec2_name}-shipping"
+  instance_type          = "t3.small"
+  vpc_security_group_ids = [data.aws_ssm_parameter.shipping_sg_id.value]
+  subnet_id              = local.private_subnet_id
+  tags = merge(
+    var.common_tags,
+    {
+      Component = "shipping"
+    },
+    {
+      Name = "${local.ec2_name}-shipping"
+    }
+  )
+}
+
+module "payment" {
+  source                 = "terraform-aws-modules/ec2-instance/aws"
+  ami = data.aws_ami.centos8.id
+  name                   = "${local.ec2_name}-payment"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [data.aws_ssm_parameter.payment_sg_id.value]
+  subnet_id              = local.private_subnet_id
+  tags = merge(
+    var.common_tags,
+    {
+      Component = "payment"
+    },
+    {
+      Name = "${local.ec2_name}-payment"
+    }
+  )
+}
+
+module "web" {
+  source                 = "terraform-aws-modules/ec2-instance/aws"
+  ami = data.aws_ami.centos8.id
+  name                   = "${local.ec2_name}-web"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [data.aws_ssm_parameter.web_sg_id.value]
+  subnet_id              = local.public_subnet_id
+  tags = merge(
+    var.common_tags,
+    {
+      Component = "web"
+    },
+    {
+      Name = "${local.ec2_name}-web"
+    }
+  )
+}
