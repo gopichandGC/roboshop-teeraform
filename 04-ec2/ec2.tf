b930313d5824c8836ef1a92ac705d78194a756ea
gopichandGC <gopichandvadlamudi123@gmail.com>
2024-02-27 20:24:32 +0530

    Ec2 Servers Creation For Roboshop Project

--- 04-ec2/ec2.tf
+++ 04-ec2/ec2.tf
@@ -15,3 +15,57 @@ module "mongodb" {
     }
   )
 }
+
+module "redis" {
+  source                 = "terraform-aws-modules/ec2-instance/aws"
+  ami = data.aws_ami.centos8.id
+  name                   = "${local.ec2_name}-redis"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [data.aws_ssm_parameter.redis_sg_id.value]
+  subnet_id              = local.database_subnet_id
+  tags = merge(
+    var.common_tags,
+    {
+      Component = "redis"
+    },
+    {
+      Name = "${local.ec2_name}-redis"
+    }
+  )
+}
+
+module "mysql" {
+  source                 = "terraform-aws-modules/ec2-instance/aws"
+  ami = data.aws_ami.centos8.id
+  name                   = "${local.ec2_name}-mysql"
+  instance_type          = "t3.small"
+  vpc_security_group_ids = [data.aws_ssm_parameter.mysql_sg_id.value]
+  subnet_id              = local.database_subnet_id
+  tags = merge(
+    var.common_tags,
+    {
+      Component = "mysql"
+    },
+    {
+      Name = "${local.ec2_name}-mysql"
+    }
+  )
+}
+
+module "rabbitmq" {
+  source                 = "terraform-aws-modules/ec2-instance/aws"
+  ami = data.aws_ami.centos8.id
+  name                   = "${local.ec2_name}-rabbitmq"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [data.aws_ssm_parameter.rabbitmq_sg_id.value]
+  subnet_id              = local.database_subnet_id
+  tags = merge(
+    var.common_tags,
+    {
+      Component = "rabbitmq"
+    },
+    {
+      Name = "${local.ec2_name}-rabbitmq"
+    }
+  )
+}
